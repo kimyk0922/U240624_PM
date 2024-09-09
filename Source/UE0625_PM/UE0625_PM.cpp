30299a5f913d26cdffa3e085bde86992ec4045fe
psy_duck <kimyk0922@gmail.com>
2024-09-09 15:50:26 +0900

    add

---
+++ Source/UE0625_PM/UE0625_PM.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE0625_PM.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE0625_PM, "UE0625_PM" );
